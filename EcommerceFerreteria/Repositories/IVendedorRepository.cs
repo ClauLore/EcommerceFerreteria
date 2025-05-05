d68ba3318c35940dfbf6cbbc41929a90b563e957
Claudia <claudialo.garciar@gmail.com>
2025-05-05 11:10:13 -0500

    Agregando repositorio Vendedor

---
+++ EcommerceFerreteria/Repositories/IVendedorRepository.cs
@@ -0,0 +1,16 @@
+﻿using EcommerceFerreteria.Modelos;
+using EcommerceFerreteria.Modelos.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceFerreteria.Repositories
+{
+    public interface IVendedorRepository:IRepository<Vendedor>
+    {
+        Vendedor ObtenerPorDNI(string dni);
+        List<Vendedor> ObtenerPorEstado(Estado estado);
+    }
+}
---
+++ EcommerceFerreteria/Repositories/VendedorRepository.cs
@@ -0,0 +1,54 @@
+﻿using EcommerceFerreteria.Modelos;
+using EcommerceFerreteria.Modelos.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceFerreteria.Repositories
+{
+    public class VendedorRepository : IVendedorRepository
+    {
+        private List<Vendedor> _vendedores = new List<Vendedor>();
+        void IRepository<Vendedor>.Actualizar(Vendedor entidad)
+        {
+           var index = _vendedores.FindIndex(v=> v.Id==entidad.Id);
+           if (index != -1)
+           {
+                _vendedores[index] = entidad;
+           }
+        }
+
+        void IRepository<Vendedor>.Agregar(Vendedor entidad)
+        {
+            _vendedores.Add(entidad);
+        }
+
+        void IRepository<Vendedor>.Eliminar(Vendedor entidad)
+        {
+            _vendedores.Remove(entidad);
+        }
+
+        Vendedor IVendedorRepository.ObtenerPorDNI(string dni)
+        {
+            return _vendedores.FirstOrDefault(v => v.DNI == dni);
+        }
+
+        List<Vendedor> IVendedorRepository.ObtenerPorEstado(Estado estado)
+        {
+            return _vendedores.Where(v => v.Estado == estado).ToList();
+        }
+
+        Vendedor IRepository<Vendedor>.ObtenerPorId(int id)
+        {
+            return _vendedores.FirstOrDefault(v => v.Id == id);
+        }
+
+        List<Vendedor> IRepository<Vendedor>.ObtenerTodo()
+        {
+           return _vendedores;
+        }
+    }
+}
