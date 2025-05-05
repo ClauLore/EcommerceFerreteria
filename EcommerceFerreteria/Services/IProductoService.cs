1f155768f526b2cc6c8481ec6075449865e70d37
Claudia <claudialo.garciar@gmail.com>
2025-05-05 13:10:37 -0500

    Agregando Servicio Producto

---
+++ EcommerceFerreteria/Services/IProductoService.cs
@@ -0,0 +1,18 @@
+﻿using EcommerceFerreteria.Modelos.Enums;
+using EcommerceFerreteria.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceFerreteria.Services
+{
+    internal interface IProductoService
+    {
+        Producto CrearProducto(string descripcion, decimal precio, int stock,Estado estado, CategoriaProducto categoria);
+        Producto DeshabilitarProducto(int IdProducto);
+        List<Producto> ObtenerPorCategoria(CategoriaProducto categoria);
+        List<Producto> ProductosConStock();
+    }
+}
---
+++ EcommerceFerreteria/Services/ProductoService.cs
@@ -0,0 +1,67 @@
+﻿using EcommerceFerreteria.Modelos;
+using EcommerceFerreteria.Modelos.Enums;
+using EcommerceFerreteria.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceFerreteria.Services
+{
+    public class ProductoService : IProductoService
+    {
+        private readonly IVentaRepository _ventaRepository;
+        private readonly IProductoRepository _productoRepository;
+
+        public ProductoService(
+        IProductoRepository productoRepository,
+        IVentaRepository ventaRepository)
+        {
+            this._productoRepository = productoRepository;
+            this._ventaRepository = ventaRepository;
+        }
+        public List<Producto> ObtenerPorCategoria(CategoriaProducto categoria)
+        {
+            return _productoRepository.ObtenerPorCategoria(categoria);
+        }
+
+        public List<Producto> ProductosConStock()
+        {
+            return _productoRepository.ProductosConStock();
+        }
+
+        Producto IProductoService.DeshabilitarProducto(int IdProducto)
+        {
+            var producto = _productoRepository.ObtenerPorId(IdProducto);
+            producto.Estado = Estado.INACTIVO;
+            _productoRepository.Actualizar(producto);
+
+            return producto;
+        }
+
+        Producto IProductoService.CrearProducto(string descripcion, decimal precio, int stock, Estado estado, CategoriaProducto categoria)
+        {
+
+           var producto = new Producto
+                {
+                 Descripcion = descripcion,
+                 Precio = precio,
+                 Stock = stock,
+                 Estado = estado,
+                 CategoriaProducto = categoria
+                };
+                _productoRepository.Agregar(producto);
+            
+            return producto;
+        }
+
+        List<Producto> IProductoService.ObtenerPorCategoria(CategoriaProducto categoria)
+        {
+            return _productoRepository.ObtenerPorCategoria(categoria);
+        }
+
+     
+    }
+}
