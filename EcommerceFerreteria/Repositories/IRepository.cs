74b6b99caeef7e3406a396b8266409410a99d263
Claudia <claudialo.garciar@gmail.com>
2025-05-05 10:36:37 -0500

    Agregando repositorio genérico

---
+++ EcommerceFerreteria/Repositories/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceFerreteria.Repositories
+{
+    public interface IRepository<T>
+    {
+        List<T> ObtenerTodo();
+        T ObtenerPorId(int id);
+        void Agregar(T entidad);
+        void Actualizar(T entidad);
+        void Eliminar(T entidad);
+
+    }
+}
